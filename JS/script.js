04a37add580b68b6926b283e1bb5fb5a6257f2f2
Mariusz Lyszcz <mariuszly@gmail.com>
2022-12-03 17:20:55 +0100

    const instead let in JS

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,15 @@
 {
     const toggleBackground = () => {
-        let body = document.querySelector(".body");
-        let button = document.querySelector(".button")
-        let themeName = document.querySelector(".themeName");
+        const body = document.querySelector(".body");
+        const button = document.querySelector(".button")
+        const themeName = document.querySelector(".themeName");
         body.classList.toggle("blue");
         button.classList.toggle("green");
         themeName.innerText = body.classList.contains("blue") ? "zielone" : "niebieskie";
     }
     
     const init = () => {
-        let button = document.querySelector(".button")
+        const button = document.querySelector(".button")
         button.addEventListener("click", toggleBackground);
     }
 
